d2248b354b95e86578f4cc42ba5f6f9344af251d
Eunjae Lee <karis612@gmail.com>
2019-09-01 21:08:21 +0200

    remove onCreateNode from examples

--- examples/blog/gatsby-node.js
+++
@@ -1,2 +0,0 @@
-const { onCreateNode } = require('gatsby-theme-mdx-blog');
-exports.onCreateNode = onCreateNode;
--- examples/tailwind/gatsby-node.js
+++
@@ -1,4 +0,0 @@
-const { onCreateNode } = require('gatsby-theme-mdx-blog');
-exports.onCreateNode = ({ node, actions, loaders, getNode }) => {
-  onCreateNode({ node, actions, getNode });
-};
